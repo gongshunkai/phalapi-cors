50b096c72268d0372aa125f91488d59f8a5ab4ce
gongshunkai <49078111@qq.com>
2018-11-23 10:39:05 +0800

    cors

--- config/app.php
+++ config/app.php
@@ -1,13 +1,10 @@
-# CORS跨域扩展
+<?php
+
+return array(
+
+    //请将以下配置拷贝到 ./Config/app.php 文件中
 
 
-## 安装和配置
-修改项目下的composer.json文件，并添加：  
-```
-    "phalapi/cors":"dev-master"
-```
-在/path/to/phalapi/config/app.php文件中，配置： 
-```
     'cors' => array(
         //域名白名单
         'whitelist'   => array(
@@ -20,14 +17,4 @@
             'Access-Control-Allow-Credentials' => 'true' //支持cookie
         )
     )
-```
-然后执行```composer update```。  
-
-## 注册
-在/path/to/phalapi/config/di.php文件中，注册：  
-```php
-
-$di->cors = function() {
-	return new \PhalApi\CORS\Lite();
-};
-```
+);
---
+++ src/Lite.php
@@ -0,0 +1,43 @@
+<?php
+namespace PhalApi\CORS;
+
+/**
+ * 2018/11/23 CORS跨域拓展 @吞吞小猴<49078111@qq.com>
+ */
+
+class Lite {
+
+    protected $config = array(
+
+        //域名白名单
+        'whitelist' => array(),
+
+        //header头
+        'headers' => array()
+    );
+
+    protected $flag = false;
+    
+    public function __construct() {
+        if(\PhalApi\DI()->config->get('app.cors'))
+            $this->config = array_merge($this->config, \PhalApi\DI()->config->get('app.cors'));
+
+        $origin = \PhalApi\DI()->request->getHeader('Origin');
+
+        foreach ($this->config['whitelist'] as $val) {
+            if($origin == $val){
+                $this->flag = true;
+            }
+        }
+
+        if($this->flag){
+
+            $this->config['headers']['Access-Control-Allow-Origin'] = $origin;
+            $this->config['headers']['Access-Control-Allow-Headers'] = 'Content-Type';
+
+            foreach ($this->config['headers'] as $key => $val) {
+                 \PhalApi\DI()->response->addHeaders($key,$val);
+            }
+        }
+    }
+}
